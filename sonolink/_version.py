baa47a91a40b0e54ef4fb43880424d96493f6b1e
Michael <vmphase.dev@gmail.com>
2026-09-16 21:58:54 +0200

    chore: bump changelog and _version to 1.4.0 (#120)

--- sonolink/_version.py
+++ sonolink/_version.py
@@ -44,5 +44,5 @@ class VersionInfo(NamedTuple):
         return base + suffix
 
 
-version_info = VersionInfo(major=1, minor=4, patch=0, release_level="alpha")
+version_info = VersionInfo(major=1, minor=4, patch=0, release_level="final")
 __version__ = version_info.as_str()
